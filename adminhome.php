17733053b8c858aa14e2aff9782e26bfa869cc5a
HenryGardiner <gardiner.hcw@oundleschool.org.uk>
2021-06-30 11:41:47 +0100

    login takes to homes

---
+++ adminhome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Admin Home</title>
+    
+</head>
+<body>
+
+<h1>Admin</h1>
+
+<!--button that sends the admin to the add user page-->
+<form action="adduser.php" method="get">
+    <input type="submit" value="Add a user">
+</form>
+
+</body>
+</html>
--- loginprocess.php
+++ loginprocess.php
@@ -1,4 +1,8 @@
 <?php
+//removes any existing session and starts a new one
+session_start(); 
+session_destroy();
+session_start();
 
 print_r($_POST); //prints the posted values - useful for debugging
 
@@ -15,12 +19,19 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
     $attempt=$_POST['password'];
     if (password_verify($attempt,$hash)) { //this checks the hash of the inputted 
         //password against the hash of the actual password in the database
-
-        echo('login successful');
-    }else{
-        echo('incorrect password ');
         
-    }
+        //sets session variables
+        $_SESSION['srole']=$row['role']; 
+        $_SESSION['suser']=$row['username'];
+        
+        //sends user to respective home pages
+        if($row['role']== 2){
+            header('Location: adminhome.php');
+        }elseif($row['role']== 1){
+            header('Location: tutorhome.php');
+        }else{
+            header('Location: pupilhome.php');
+        }
 }
 
 
